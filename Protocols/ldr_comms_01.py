0e5f3de460e1eb7828940678ec3becb4bf5626a4
LuhyLuhan <luhanolivier20@gmail.com>
2024-12-29 18:52:48 +0200

    Update ldr_comms_01.py
    Made the code read a letter sent to it

--- Protocols/ldr_comms_01.py
+++ Protocols/ldr_comms_01.py
@@ -1,5 +1,8 @@
 from microbit import*
 s = 500
+r = 0
+t = 0
+x = 0
 code = 0
 cod = 0
 b = pin0
@@ -34,6 +37,7 @@ X = [1,1,1,0,1,1,1,1]
 Y = [1,1,1,1,0,1,1,1]
 Z = [1,1,1,1,1,1,1,1]
 mors = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+mor = []
 #This variable will contain the sentance that you want to send
 let = hello
 txt = let.upper()
@@ -41,7 +45,7 @@ text = txt.split()
 num = len(text)
 mun = len(A)
 print(num*mun)
-val = b.read_digital()
+val = b.read_analogl()
 for x in range(num*mun):
     led.write_digital(let[code][cod])
     if cod == 7:
@@ -53,3 +57,41 @@ for x in range(num*mun):
         code += 1
     cod += 1
     sleep(s)
+    while x != 1:
+        val = b.read_analog()
+        if ldr >= 800:
+            x = 1
+            mor.append(x)
+            sleep(1)
+    while x == 1:
+        val = b.read_analog()
+        t += 1
+        sleep(1)
+        if val >= 800:
+            if 450 <= t >= 490:
+               mor.append(x)
+                sleep(1)
+            elif 850 <= t >= 990:
+                mor.append(r)
+                mor.append(x)
+                sleep(1)
+            elif 1350 <= t >= 1490:
+                mor.append(r)
+                mor.append(r)
+                mor.append(x)
+                sleep(1)
+            elif 1850 <= t >= 1990:
+                for x in range(3):
+                    mor.append(r)
+                mor.append(x)
+                sleep(1)
+            elif 2750 <= t >= 3000:
+                for x in range(6):
+                    mor.append(r)
+                mor.append(x):
+                sleep(1)
+        len = len(mor)
+        if len == 8:
+            x = 0
+            sleep(s)
+print(mor)
